a5cad62bef1d25a1d11db9f87d7f204e2cebfa5c
Jonathan Kolman <jonathankolman@Jonathans-MBP.fios-router.home>
2022-03-01 07:56:03 -0500

    debugging

--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -5,7 +5,10 @@ const router = Router()
 
 router.route('/sendToDevice')
   .post((req) => {
+		console.log('got request', req)
   	const { messageData, deviceToken } = req.body
+		console.log('message data', messageData)
+		console.log('device token', deviceToken)
   	admin.messaging().send({
 	    token: deviceToken,
 	    data: messageData
